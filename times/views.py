27213ddbd4194284a2ab6f77849938ea54495363
Caio Gomes <caiogdo2323@gmail.com>
2026-04-02 23:17:25 -0300

    feat: implement team listing with template inheritance and Bootstrap
    created base.html, integrated Bootstrap 5, configured view logic and app routing for team list

--- times/views.py
+++ times/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Time
 
-# Create your views here.
+def listar_times(request):
+    # Buscamos todos os times cadastrados
+    times = Time.objects.all()
+    # Enviamos para o template dentro de um dicionário
+    return render(request, 'times/listar_times.html', {'times': times})
