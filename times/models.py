24666c13ca4eb807e784f9216e21e90abbfea284
Caio Gomes <caiogdo2323@gmail.com>
2026-04-02 22:09:29 -0300

    feat: add Time model and register in admin
    create model, apply migrations and enable admin interface

--- times/models.py
+++ times/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Time(models.Model):
+    nome = models.CharField(max_length=100)
+    ano_fundacao = models.IntegerField()
+    pais = models.CharField(max_length=100)
+    estado = models.CharField(max_length=100)
+    cidade = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.nome} ({self.cidade} - {self.estado}, {self.pais}) - Fundado em {self.ano_fundacao}"
